4b562494fe082493c601e69ea7064d7dc9a332d6
Jakub Stanislawczyk <jakubstanislawczyk98@gmail.com>
2018-08-21 20:48:50 +0200

    Calling js code in react component

--- react/01-first-app/src/App.js
+++ react/01-first-app/src/App.js
@@ -2,10 +2,22 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+
+  constructor() {
+    super();
+    this.name = this.getText();
+
+  }
+  
+  getText() {
+    return 'Kuba';
+  }
+  
   render() {
     return (
       <div className="App">
         <h1>First React App</h1>
+        <h2>{this.getText()}</h2>
       </div>
     );
   }
