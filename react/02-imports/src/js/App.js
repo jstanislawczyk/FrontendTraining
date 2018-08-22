3d9d2ac4c61950a3dfe38755d685356cc0d6db17
Jakub Stanislawczyk <jakubstanislawczyk98@gmail.com>
2018-08-22 22:08:02 +0200

    imports

---
+++ react/02-imports/src/js/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import '../css/App.css';
+
+import Header from './Header.js';
+
+class App extends Component {
+
+  render() {
+    return (
+      <div>
+        <Header />
+      </div>  
+    );
+  }
+}
+
+export default App;
---
+++ react/02-imports/src/js/Header.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+  render() {
+    return (
+      <header>Test header</header>
+    );
+  }
+}
+
+export default Header;
