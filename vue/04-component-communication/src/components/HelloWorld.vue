937b8a9962655c4c7a20654de52521baa56801b6
Jakub Stanislawczyk <jakubstanislawczyk98@gmail.com>
2018-09-11 10:18:16 +0200

    props

---
+++ vue/04-component-communication/src/components/HelloWorld.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <h1>{{ hello }}</h1>
+    <h2 v-if="!seen">You can see this</h2>
+    <h2 v-if="seen">This is hidden</h2>
+
+    <li v-for="text in structure">
+      {{ text.text }}
+    </li>
+
+    <br />
+    <button v-on:click="logInfo">Click me</button>
+    <br />
+    <router-link to="/test"><button>Go to Test</button></router-link>
+    <global-test-component></global-test-component>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Hello',
+  data() {
+    return {
+      seen: false,
+      hello: 'Hello world',
+      structure: [
+        { text: 'text 1' },
+        { text: 'text 2' },
+        { text: 'text 3' }
+      ] 
+    }
+  },
+  methods: {
+    logInfo: function() {
+      console.log(123);
+    }
+  }
+}
+</script>
+
+<style>
+  h1, h2 {
+    color: green;
+  }
+</style>
---
+++ vue/04-component-communication/src/components/LocalComponent.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <br />
+        <p>
+            Type something: <input type="text" :value="msg" @input="changeMessage">
+        </p>
+        <p>Message: {{ message }}</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: ['msg'],
+        data() {
+            return {
+                message: ''
+            }
+        },
+        methods: {
+            changeMessage(event) {
+                this.message = event.target.value;
+                this.$emit('messageChange', this.message);
+            }
+        }
+    }
+</script>
